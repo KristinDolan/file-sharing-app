4ea2aeaf78b732496fa21a075e5d69eb7dd65006
Kristin Hill <krstinhill4211@gmail.com>
2024-04-13 19:50:08 -0400

    registration form functionality added

--- app.py
+++ app.py
@@ -29,6 +29,10 @@ def index():
 def login():
     return render_template('login.html')
 
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
 @app.route('/file/upload')
 def file_upload():
     return render_template('file_upload.html')
